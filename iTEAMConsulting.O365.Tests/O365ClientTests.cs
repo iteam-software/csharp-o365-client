2020090f72f0b05dd1929ae63468325997353288
Christopher Salinas <csalinas@iteamnm.com>
2017-07-06 15:59:52 -0600

    test: initial implementation of tests project

---
+++ iTEAMConsulting.O365.Tests/O365ClientTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.Extensions.Options;
+using Microsoft.Extensions.Logging;
+using Moq;
+using Xunit;
+
+namespace iTEAMConsulting.O365.Tests
+{
+    public class O365ClientTests
+    {
+        private IOptions<O365AuthenticationOptions> MockOptions()
+        {
+            var options = new Mock<IOptions<O365AuthenticationOptions>>();
+            options.Setup(i => i.Value).Returns(
+                new O365AuthenticationOptions()
+                {
+                    ClientId = "LKAMFLDSNFLASDFLANDF",
+                    ClientSecret = "AKLSJFLASNDFLASNDF",
+                    TenantId = "ATAETAERADFADFGER",
+                    TenantName = "FASLALKDFNLANDF"
+                }
+            );
+            return options.Object;
+        }
+
+        private ILoggerFactory MockLoggerFactory(ILogger<O365Client> logger = null)
+        {
+            var mockLogger = new Mock<ILoggerFactory>();
+            mockLogger.Setup(factory => factory.CreateLogger<O365Client>())
+                .Returns(logger);
+            return mockLogger.Object;
+        }
+    }
+}
