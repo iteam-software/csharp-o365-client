b935695acbc55ec283a73a55efbed2c6cc689af9
Matt Lynam <mlynam@iteamnm.com>
2017-07-06 15:35:45 -0600

    feat: add factory implementations

---
+++ iTEAMConsulting.O365/AdalFactory.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.IdentityModel.Clients.ActiveDirectory;
+
+namespace iTEAMConsulting.O365
+{
+    public class AdalFactory : IAdalFactory
+    {
+        public AuthenticationContext CreateAuthenticationContext(string authority)
+        {
+            return new AuthenticationContext(authority, false);
+        }
+    }
+}
---
+++ iTEAMConsulting.O365/BackchannelFactory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Net.Http;
+
+namespace iTEAMConsulting.O365
+{
+    public class BackchannelFactory : IBackchannelFactory
+    {
+        public HttpClient CreateBackchannel(string baseAddress)
+        {
+            var client = new HttpClient();
+            client.Timeout = TimeSpan.FromSeconds(30);
+            client.BaseAddress = new Uri(baseAddress);
+
+            return client;
+        }
+    }
+}
